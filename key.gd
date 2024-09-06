244ba5b2313c9d76998cddae5f82f9097630d1e6
ThatLukeDev <luke@l2j.co.uk>
2024-09-06 08:34:15 +0100

    encode tab

--- key.gd
+++ key.gd
@@ -1,22 +1,61 @@
 extends Button
 
+var rng = RandomNumberGenerator.new()
+
 var dividertime: float = 0.2
 var cleartime: float = 0.5
 
 var sincelastTime: float = 0
 var msg: String = ""
+var letter: int = 0
+
+var morses = [
+	".-",
+	"-...",
+	"-.-.",
+	"-..",
+	".",
+	"..-.",
+	"--.",
+	"....",
+	"..",
+	".---",
+	"-.-",
+	".-..",
+	"--",
+	"-.",
+	"---",
+	".--.",
+	"--.-",
+	".-.",
+	"...",
+	"-",
+	"..-",
+	"...-",
+	".--",
+	"-..-",
+	"-.--",
+	"--..",
+]
+
+func chooserand() -> void:
+	letter = rng.randi_range(0, 25)
+	$"../title".text = char(65 + letter) + "\n "
+	pass
 
 func _ready() -> void:
-	pass
+	chooserand()
 
 func _process(delta: float) -> void:
 	sincelastTime += delta
 	
 	if sincelastTime > cleartime:
+		if msg == " " + morses[letter]:
+			chooserand()
 		msg = ""
 	
 	if msg:
-		$".".text = "\n" + msg + "\n "
+		$".".text = "\n" + msg.replace(".", "·").replace("-", "−") + "\n "
 	else:
 		$".".text = "\n\n\n "
 
@@ -27,7 +66,7 @@ func _on_button_down() -> void:
 
 func _on_button_up() -> void:
 	if sincelastTime > dividertime:
-		msg += "−"
+		msg += "-"
 	else:
-		msg += "·"
+		msg += "."
 	sincelastTime = 0
